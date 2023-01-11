13351398ac5c2b9e8fe9e0b4595817073f1d4227
Javi <javier@typeform.com>
2023-01-11 13:13:20 +0100

    test: Foo returns Foo

--- src/tests/camelcase.test.ts
+++ src/tests/camelcase.test.ts
@@ -4,4 +4,7 @@ describe('camel case', () => {
 	it('should return empty string when passing ""', () => {
 		expect(camelCase('')).toBe('');
 	});
+	it('should return capitalised string when passing capitalized word', () => {
+		expect(camelCase('Foo')).toBe('Foo');
+	});
 });
