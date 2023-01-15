a7913c07f67b96cf3d9d42bf318b247819819556
Javi <javier@typeform.com>
2023-01-15 20:27:50 +0100

    refactor: extract word splitter

--- src/core/camelcase.ts
+++ src/core/camelcase.ts
@@ -1,10 +1,14 @@
 export function capitaliseFirstLetter(word:string) {
     return word.charAt(0).toUpperCase() + word.substring(1, word.length).toLowerCase();
 }
-export function camelCase (phrase:string) {
-    phrase = phrase.split(' ').join('_')
-    phrase = phrase.split('-').join('_')
-    let words = phrase.split('_')
 
+function removeSeparators(phrase: string) {
+    return phrase.split(/[ _-]/);
+}
+
+export function camelCase (phrase:string) {
+    let words = removeSeparators(phrase)
     return words.map((word) => capitaliseFirstLetter(word)).join('')
 }
+
+
